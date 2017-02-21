2bb3ad8a85c19e5ab8eea96dae3147b7bd2dd326
zhiqiang.wu <zhiqiang.wu@chukou1.com>
2017-02-21 19:25:52 +0800

    add nosql entity

---
+++ Models/State.cs
@@ -0,0 +1,16 @@
+using ArkApplication.Framework.NoSql;
+
+namespace ArkApplication.Models
+{
+  
+    public class State : Entity
+    {
+         
+         public string Abbreviation { get; set; }
+
+         public string Name { get; set; }
+
+    }  
+
+
+}
